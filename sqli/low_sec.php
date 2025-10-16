6262cfd6a90580a4a1091faf4637d056f2409b68
Jehan404 <jehan.irani23@spit.ac.in>
2025-10-16 21:53:05 +0530

    Update low_sec.php

--- sqli/low_sec.php
+++ sqli/low_sec.php
@@ -1,14 +1,36 @@
 <?php
-// file to modify: /var/www/html/dvwa/vulnerabilities/xss_r/source/low.php
+// file to modify: /var/www/html/dvwa/vulnerabilities/sqli/source/low.php
+if( isset( $_GET[ 'Submit' ] ) ) {
+    // Get input
+    $id = $_GET[ 'id' ];
 
-header ("X-XSS-Protection: 0");
+    // Prepare the statement
+    // The '?' is a placeholder for our user input.
+    $query = "SELECT first_name, last_name FROM users WHERE user_id = ? LIMIT 1;";
+    $stmt = mysqli_prepare($GLOBALS["___mysqli_ston"], $query);
 
-// Is there any input?
-if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
-    // We wrap the user input with htmlspecialchars() to prevent XSS.
-    // This function converts special characters like '<' and '>' into their
-    // safe HTML entity equivalents (&lt; and &gt;).
-    $html .= '<pre>Hello ' . htmlspecialchars( $_GET[ 'name' ] ) . '</pre>';
+    // Bind the user input to the placeholder.
+    // The "s" indicates that the variable is a string. The DB driver handles it safely.
+    mysqli_stmt_bind_param($stmt, "s", $id);
+
+    // Execute the prepared statement
+    mysqli_stmt_execute($stmt);
+
+    // Get the results
+    $result = mysqli_stmt_get_result($stmt);
+
+    // Get results
+    while( $row = mysqli_fetch_assoc( $result ) ) {
+        // Get values
+        $first = $row["first_name"];
+        $last  = $row["last_name"];
+
+        // Feedback for end user
+        echo "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+    }
+
+    // Close the statement and the connection
+    mysqli_stmt_close($stmt);
+    mysqli_close($GLOBALS["___mysqli_ston"]);
 }
-
 ?>
